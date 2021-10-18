7b0bfbeaa411b2a13c604e035d98f195e6c4acc7
zzq199747 <283891510@qq.com>
2021-10-18 15:00:45 +0800

    构建项目、配置全局样式、路由、element-pls

---
+++ mytest/src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="login">
+      <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    </div>
+</template>
+<script>
+    export default{
+        name:'login'
+    }
+</script>
+<style>
+#login{
+    height: 100vh;
+    background-color: cornflowerblue;
+}
+</style>
